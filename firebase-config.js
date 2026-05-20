345c7a099963600f7dff079527ef5f53d83b339c
Developer <dev@example.com>
2026-05-20 16:26:01 +0900

    Firebase 設定を更新し、正しい API キーとプロジェクト情報を追加しました

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.__FIREBASE_CONFIG__ = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyD2jqw7F0ayfc2JyUsweWVtYhwNJvuhmR8",
+  authDomain: "schedule-web-app-9e829.firebaseapp.com",
+  projectId: "schedule-web-app-9e829",
+  storageBucket: "schedule-web-app-9e829.firebasestorage.app",
+  messagingSenderId: "640202250349",
+  appId: "1:640202250349:web:02a9b94bfe09ab8c7e0764",
 };
